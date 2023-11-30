7d1816a5e125b3feff305dbea3d8438b9794b32c
sachin-maurya <sachinmaurya1710@gmail.com>
2023-11-30 10:05:55 +0530

    added main section

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,25 @@ const fadeEffect = () => {
 const fadeInterval = setInterval(fadeEffect, 300);
 
 window.addEventListener('load', fadeEffect);
+
+
+// SCROLL TRIGGer
+
+const viewportHeader = document.querySelector('.intro');
+
+// Function to handle the scroll event
+const handleScroll = () => {
+    const scaleFactor = 1 - window.scrollY / window.innerHeight;
+
+    viewportHeader.style.transform = `scale(${scaleFactor})`;
+    viewportHeader.style.opacity = scaleFactor;
+
+    if (scaleFactor <= 0) {
+        viewportHeader.style.display = 'none';
+    } else {
+        viewportHeader.style.display = 'flex';
+    }
+};
+
+window.addEventListener('scroll', handleScroll);
+handleScroll();
