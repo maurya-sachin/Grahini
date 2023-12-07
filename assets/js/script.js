f2c4667a4bb6e9ca878b11e7bdf2fd09c3e05f1a
sachin-maurya <sachinmaurya1710@gmail.com>
2023-12-07 15:22:14 +0530

    added functionality for login/logout

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,6 @@ document.addEventListener("DOMContentLoaded", function () {
     intro.classList.add("animate-intro");
   }, 3000);
 
-
-
-
-
-
   // Get the header element
   let header = document.querySelector('.header');
 
@@ -67,8 +62,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-
-
   // Function to toggle the sidebar
   function toggleSidebar() {
     if (isHeaderVisible) {
@@ -85,7 +78,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Add a click event listener to the menu button
   menuButton.addEventListener('click', toggleSidebar);
-
 
   let footer = document.querySelector('footer');
   let isContentVisible = false;
@@ -108,106 +100,192 @@ document.addEventListener("DOMContentLoaded", function () {
       opacity: isContentVisible ? 1 : 0,
       ease: "power2.out"
     });
-
   });
 
+  // Section horizontal scroll
+  const horizontalSections = gsap.utils.toArray('section.horizontal');
 
-});
+  horizontalSections.forEach(function (sec, i) {
+    let thisPinWrap = sec.querySelector('.pin-wrap');
+    let thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');
 
+    let getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth);
 
-// Section horizontal scroll
+    gsap.fromTo(thisAnimWrap, {
+      x: () => thisAnimWrap.classList.contains('to-right') ? 0 : getToValue()
+    }, {
+      x: () => thisAnimWrap.classList.contains('to-right') ? getToValue() : 0,
+      ease: "none",
+      scrollTrigger: {
+        trigger: sec,
+        start: "top top+=100",
+        end: () => "+=" + (thisAnimWrap.scrollWidth - window.innerWidth),
+        pin: thisPinWrap,
+        invalidateOnRefresh: true,
+        scrub: true,
+      }
+    });
+  });
 
-const horizontalSections = gsap.utils.toArray('section.horizontal')
+  // Add an event listener to the login/logout button
+  const loginButton = document.querySelector(".login-button");
+  loginButton.addEventListener("click", toggleLoginLogout);
 
-horizontalSections.forEach(function (sec, i) {
+  // Check if the user is logged in during page load
+  const user = JSON.parse(localStorage.getItem("user"));
+  updateLoginStatus(user);
 
-  let thisPinWrap = sec.querySelector('.pin-wrap');
-  let thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');
+  function toggleLoginLogout() {
 
-  let getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth);
-
-  gsap.fromTo(thisAnimWrap, {
-    x: () => thisAnimWrap.classList.contains('to-right') ? 0 : getToValue()
-  }, {
-    x: () => thisAnimWrap.classList.contains('to-right') ? getToValue() : 0,
-    ease: "none",
-    scrollTrigger: {
-      trigger: sec,
-      start: "top top+=100",
-      end: () => "+=" + (thisAnimWrap.scrollWidth - window.innerWidth),
-      pin: thisPinWrap,
-      invalidateOnRefresh: true,
-      //anticipatePin: 1,
-      scrub: true,
-      //markers: true,
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      // If logged in, perform logout
+      logout();
+    } else {
+      // If not logged in, show/hide login box
+      toggleButtons();
     }
-  });
-
-
-
-
-
-
-
-  // Function to open the login form
-  function openForm() {
-    var loginBox = document.getElementById("login");
-    loginBox.style.display = 'block';
-    setTimeout(function () {
-      loginBox.style.opacity = 1;
-    }, 10);
   }
 
-  // Function to close the login form
-  function closeForm() {
-    var loginBox = document.getElementById("login");
-    loginBox.style.opacity = 0;
+  const logoutPopup = document.getElementById("logoutPopup");
+
+  function logout() {
+    // Clear user data from localStorage
+    localStorage.removeItem("user");
+
+    // Update UI to reflect logout
+    document.getElementById("logoutResultParagraph").innerText = "Logout successful. Goodbye!";
+
+    // Open the logout popup
+    openLogoutPopup();
+
+    // Update UI to reflect logout
+    updateLoginStatus(null);
+  }
+
+  function updateLoginStatus(user) {
+    if (user) {
+      // If user is logged in
+      loginButton.innerText = "Log Out";
+      document.getElementById("userName").innerText = user.name;
+      document.getElementById("userImage").src = user.image;
+    } else {
+      // If user is not logged in
+      loginButton.innerText = "Login/Sign Up";
+      document.getElementById("userName").innerText = "Admin";
+      document.getElementById("userImage").src = "/assets/images/user.png";
+    }
+  }
+
+  function openLogoutPopup() {
+    logoutPopup.style.display = "flex";
+    logoutPopup.style.opacity = 1;
+  }
+
+  function closeLogoutPopup() {
+    logoutPopup.style.opacity = 0;
     setTimeout(function () {
+      logoutPopup.style.display = "none";
+    }, 500);
+
+    // Refresh the page
+    window.location.reload();
+  }
+
+  document.querySelector(".logoutButton").addEventListener("click", closeLogoutPopup);
+
+  var loginBoxVisible = false;
+
+  function toggleButtons() {
+    var loginBox = document.getElementById("login");
+
+    if (loginBoxVisible) {
+      // If login box is visible, hide it
+      loginBox.style.opacity = 0;
+      setTimeout(function () {
+        loginBox.style.display = 'none';
+      }, 500);
+
+      // Change login/logout button text based on login status
+      const user = JSON.parse(localStorage.getItem("user"));
+      loginButton.innerText = user ? "Log Out" : "Login/Sign Up";
+
+      // Update the flag
+      loginBoxVisible = false;
+    } else {
+      // If login box is not visible, show it
+      loginButton.innerText = "Close"; // Change login/logout button text to "Close"
+      loginBox.style.display = 'block';
+      setTimeout(function () {
+        loginBox.style.opacity = 1;
+      }, 10);
+
+      // Update the flag
+      loginBoxVisible = true;
+    }
+  }
+
+  function openPopup() {
+    let signInPopup = document.getElementById("signInPopup");
+    signInPopup.style.display = "flex";
+    signInPopup.style.opacity = 1;  // Set opacity immediately without delay
+  }
+
+  function closePopup() {
+    let signInPopup = document.querySelector(".signInPopup");
+    var loginBox = document.getElementById("login");
+    signInPopup.style.opacity = 0;
+    setTimeout(function () {
+      toggleButtons();
       loginBox.style.display = 'none';
+      signInPopup.style.display = 'none';
     }, 500);
   }
 
+  document.querySelector(".popUpButton").addEventListener("click", closePopup);
 
-  document.querySelector(".login-button").addEventListener("click", openForm);
-  document.querySelector(".close").addEventListener("click", closeForm);
-
-
-});
-
-
-// GO TO TOP
-function goToTop() {
-  // Scroll to the top of the page with a smooth animation
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-}
-
-
-
-// Function to open the modal
-function openModal() {
-  document.getElementById('disclaimerModal').style.display = 'block';
-}
-
-// Function to close the modal
-function closeModal() {
-  document.getElementById('disclaimerModal').style.display = 'none';
-}
-
-// Optional: Close the modal if the user clicks outside of it
-window.onclick = function (event) {
-  let modal = document.getElementById('disclaimerModal');
-  if (event.target === modal) {
-    modal.style.display = 'none';
+  // GO TO TOP
+  function goToTop() {
+    // Scroll to the top of the page with a smooth animation
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   }
-};
 
+  // Function to open the modal
+  function openModal() {
+    document.getElementById('disclaimerModal').style.display = 'block';
+  }
 
+  // Function to close the modal
+  function closeModal() {
+    document.getElementById('disclaimerModal').style.display = 'none';
+  }
 
-// Add an event listener to the window for the 'beforeunload' event
-window.addEventListener('beforeunload', function () {
-  // Scroll to the top of the page
-  window.scrollTo(0, 0);
+  // Optional: Close the modal if the user clicks outside of it
+  window.onclick = function (event) {
+    let modal = document.getElementById('disclaimerModal');
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  };
+
+  // Add an event listener to the window for the 'beforeunload' event
+  window.addEventListener('beforeunload', function () {
+    // Scroll to the top of the page
+    window.scrollTo(0, 0);
+  });
+
+  window.addEventListener("message", receiveMessage, false);
+
+  function receiveMessage(event) {
+    if (event.data && event.data.type === "userLoggedIn") {
+      const user = event.data.user;
+      document.getElementById("resultParagraph").innerText = `Login successful. Welcome, ${user.name}!`;
+      openPopup();
+      document.getElementById("userName").innerText = user.name;
+      document.getElementById("userImage").src = user.image;
+    }
+  }
 });
